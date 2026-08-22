2808c2a605ec44b37a8bf16e3768825dc29b19c4
clientcarex-cloud <clientcarex@gmail.com>
2026-08-22 15:03:29 +0530

    einvoice module rm

--- modules/einvoice/helpers/einvoice_helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function einvioce_module_get_templates(): array
-{
-    $ci = &get_instance();
-    $ci->load->model('templates_model');
-
-    return $ci->templates_model->getByType('einvoice');
-}
